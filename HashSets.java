cc795508e889133ea1bac5346b118cf982a98383
srinadh0405 <srinadh.nallabothula@email.com>
2024-08-05 18:23:28 +0530

    Sets user 1 done

--- HashSets.java
+++ HashSets.java
@@ -1,3 +1,21 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class HashSets{
 // Prepare Hashset object here and add person class objects in them iterate it by using for each
+       public static void main(String[] args){
+       Set<Person> p =new HashSet<>();
+
+       p.add(new Person(1,"Srinadh",2000.25,"Developer"));
+       p.add(new Person(1, "Srinadh", 2000.25, "Developer"));
+       p.add(new Person(2,"ABC",2000.25,"HR"));
+       p.add(new Person(3, "XYZ", 2000.25, "Team lead"));
+
+      for(Person pp:p){
+
+        System.out.println(pp);
+      }
+
+       
+}
 }
--- Person.java
+++ Person.java
@@ -1,3 +1,99 @@
 public class Person{
 // prepare a Person Data With Getters and Setters,Constructor,ToString and Override HashCode & Equals Methods in it
+
+     private int pid;
+     private String pname;
+     private double psalary;
+     private String pdesignation;
+     
+    public Person(int pid, String pname, double psalary, String pdesignation) {
+        this.pid = pid;
+        this.pname = pname;
+        this.psalary = psalary;
+        this.pdesignation = pdesignation;
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public String getPname() {
+        return pname;
+    }
+
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+
+    public double getPsalary() {
+        return psalary;
+    }
+
+    public void setPsalary(double psalary) {
+        this.psalary = psalary;
+    }
+
+    public String getPdesignation() {
+        return pdesignation;
+    }
+
+    public void setPdesignation(String pdesignation) {
+        this.pdesignation = pdesignation;
+    }
+
+    
+     
+
+    
+    @Override
+    public String toString() {
+        return "Person [pid=" + pid + ", pname=" + pname + ", psalary=" + psalary + ", pdesignation=" + pdesignation
+                + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + pid;
+        result = prime * result + ((pname == null) ? 0 : pname.hashCode());
+        long temp;
+        temp = Double.doubleToLongBits(psalary);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        result = prime * result + ((pdesignation == null) ? 0 : pdesignation.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Person other = (Person) obj;
+        if (pid != other.pid)
+            return false;
+        if (pname == null) {
+            if (other.pname != null)
+                return false;
+        } else if (!pname.equals(other.pname))
+            return false;
+        if (Double.doubleToLongBits(psalary) != Double.doubleToLongBits(other.psalary))
+            return false;
+        if (pdesignation == null) {
+            if (other.pdesignation != null)
+                return false;
+        } else if (!pdesignation.equals(other.pdesignation))
+            return false;
+        return true;
+    }
+     
 }
+
+
