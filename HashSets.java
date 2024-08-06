56c22b1f426634443b9685b6a231f66cc1261550
siva-245711 <sivanagendra809@gmail.com>
2024-08-06 15:24:05 +0530

    javasets

--- HashSets.java
+++ HashSets.java
@@ -1,3 +1,25 @@
+import java.util.HashSet;
+import java.util.Set;
 public class HashSets{
 // Prepare Hashset object here and add person class objects in them iterate it by using for each
+
+
+
+
+    public static void main(String[] args) {
+        
+        Set<Person> personSet = new HashSet<>();
+
+       
+        personSet.add(new Person("John Doe", 30, "john.doe@example.com"));
+        personSet.add(new Person("Jane Smith", 25, "jane.smith@example.com"));
+        personSet.add(new Person("Alice Johnson", 28, "alice.johnson@example.com"));
+        personSet.add(new Person("Bob Brown", 35, "bob.brown@example.com"));
+
+        
+        for (Person person : personSet) {
+            System.out.println(person);
+        }
+    }
 }
+
