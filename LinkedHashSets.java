fc783fae4e3cf84867bbb7739c377edad911e201
MekaManojKrishnalakshman <mekamanojkrishnalakshman@gmail.com>
2024-08-06 17:42:50 +0530

    done

--- LinkedHashSets.java
+++ LinkedHashSets.java
@@ -1,4 +1,38 @@
-public class LinkedHashSets{
-public static void main(String[]args){
-  // Preapre LinkedHashSets Object and Load Person Objects in it and iterate it using Iterator and While Loop
+import java.util.LinkedHashSet;
+import java.util.Iterator;
+
+// Define the Person class
+class Person {
+    String name;
+    int roll;
+
+    // Constructor
+    public Person(String name, int roll) {
+        this.name = name;
+        this.roll= roll;
+    }
+
+    // Override toString method for easy printing
+    @Override
+    public String toString() {
+        return "Person{name='" + name + "', roll=" + roll + '}';
+    }
+}
+
+public class LinkedHashSets {
+    public static void main(String[] args) {
+        // Create LinkedHashSet object
+        LinkedHashSet<Person> personSet = new LinkedHashSet<>();
+
+        // Add Person objects to the LinkedHashSet
+        personSet.add(new Person("Mekam", 5));
+        personSet.add(new Person("John", 7));
+        personSet.add(new Person("Mekam", 8));
+
+        // Iterate over the LinkedHashSet using an Iterator and a while loop
+        Iterator<Person> iterator = personSet.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
 }
