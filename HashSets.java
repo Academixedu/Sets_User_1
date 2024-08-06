c92768fe275a3b892f10b2483306e11ba34999c1
KOushikguptha <koushikguptha2003@gmail.com>
2024-08-06 17:33:56 +0530

    Sets_User_1

--- HashSets.java
+++ HashSets.java
@@ -1,3 +1,20 @@
-public class HashSets{
-// Prepare Hashset object here and add person class objects in them iterate it by using for each
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class HashSets {
+    public static void main(String[] args) {
+        // Create HashSet object
+        HashSet<Person> personSet = new HashSet<>();
+
+        // Add Person objects to the HashSet
+        personSet.add(new Person("Double Ismart", 15));
+        personSet.add(new Person("Devara", 27));
+        personSet.add(new Person("Pushpa", 8));
+
+        // Iterate over the HashSet using an Iterator and while loop
+        Iterator<Person> iterator = personSet.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
 }
