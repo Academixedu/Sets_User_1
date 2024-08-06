98055f4840b8062432c0d0560d4524e0c4989a77
ranjith81869 <ranjith81869@email.com>
2024-08-06 11:50:21 +0530

    Sets_user_1

--- HashSets.java
+++ HashSets.java
@@ -1,3 +1,17 @@
+import java.util.HashSet;
+import java.util.*;
+
 public class HashSets{
 // Prepare Hashset object here and add person class objects in them iterate it by using for each
+
+public static void main(String[]args){
+    Set<Person> list = new HashSet<Person>();
+    list.add(new Person("Ranjith", 20000.0, "developer"));
+    list.add(new Person("Ranjith", 20000.0, "developer"));
+    list.add(new Person("R", 20000.0, "HR"));
+
+    for(Person p:list){
+        System.out.println(p);
+    }
+}
 }
--- LinkedHashSets.java
+++ LinkedHashSets.java
@@ -1,4 +1,20 @@
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class LinkedHashSets{
 public static void main(String[]args){
   // Preapre LinkedHashSets Object and Load Person Objects in it and iterate it using Iterator and While Loop
+  Set<Person> list=new LinkedHashSet<Person>();
+  list.add(new Person("Ranjith", 200000.0, "Developer"));
+  list.add(new Person("Ranjith", 200000.0, "Developer"));
+  list.add(new Person("C", 30000.0, "HR"));
+  Iterator<Person> itr=list.iterator();
+
+  while (itr.hasNext()) {
+    System.out.println(itr.next());
+    
+  }
+
+}
 }
