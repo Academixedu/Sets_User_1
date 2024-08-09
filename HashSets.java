659d47ddca4b87d3f921242ded07a76e19e0cab8
Chaitanyabeeraka11 <chaitanyabeeraka@gmail.com>
2024-08-09 11:05:08 +0530

    done

--- HashSets.java
+++ HashSets.java
@@ -1,3 +1,52 @@
-public class HashSets{
-// Prepare Hashset object here and add person class objects in them iterate it by using for each
+
+import java.util.HashSet;
+
+class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{name='" + name + "', age=" + age + "}";
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + age;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+
+        Person person = (Person) obj;
+
+        if (age != person.age)
+            return false;
+        return name != null ? name.equals(person.name) : person.name == null;
+    }
+}
+
+public class HashSets {
+    public static void main(String[] args) {
+        HashSet<Person> personSet = new HashSet<>();
+
+        personSet.add(new Person("Alice", 30));
+        personSet.add(new Person("Bob", 25));
+        personSet.add(new Person("Charlie", 35));
+
+        for (Person person : personSet) {
+            System.out.println(person);
+        }
+    }
 }
--- Person.java
+++ Person.java
@@ -1,3 +1,52 @@
-public class Person{
-// prepare a Person Data With Getters and Setters,Constructor,ToString and Override HashCode & Equals Methods in it
+public class Person {
+
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{name='" + name + "', age=" + age + "}";
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + age;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+
+        Person person = (Person) obj;
+
+        if (age != person.age)
+            return false;
+        return name != null ? name.equals(person.name) : person.name == null;
+    }
 }
