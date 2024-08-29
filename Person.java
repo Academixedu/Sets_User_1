bed23f3a41fb7756ad2699cfe64b22313bca6d0c
kumar jyothiranjan <108728582+jyothiranjan25@users.noreply.github.com>
2024-08-29 17:21:38 +0530

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,3 +1,81 @@
+import java.util.Objects;
+
 public class Person{
 // prepare a Person Data With Getters and Setters,Constructor,ToString and Override HashCode & Equals Methods in it
+    private String name;
+    private int age;
+    private String address;
+    private String phoneNumber;
+    private String email;
+    private double salary;
+
+    public Person(String name, int age, String address, String phoneNumber, String email, double salary) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Double.compare(salary, person.salary) == 0 && Objects.equals(name, person.name) && Objects.equals(address, person.address) && Objects.equals(phoneNumber, person.phoneNumber) && Objects.equals(email, person.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, address, phoneNumber, email, salary);
+    }
 }
