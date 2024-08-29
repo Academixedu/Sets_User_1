8a4178bd11160cdb3dd2ce383e3ba6ca5906fbcd
Mohammad Shahid Afridi <142991991+Shahid-763@users.noreply.github.com>
2024-08-29 18:06:42 +0530

    shahid

--- HashSets.java
+++ HashSets.java
@@ -1,3 +1,13 @@
+import java.util.*;
 public class HashSets{
 // Prepare Hashset object here and add person class objects in them iterate it by using for each
+public static void main(String[] args) {
+    
+
+Set<Person>m=new HashSet<>();
+m.add(new Person(1,"hello",1000000));
+for(Person k:m){
+    System.out.println(k);
+}
+}
 }
--- LinkedHashSets.java
+++ LinkedHashSets.java
@@ -1,4 +1,13 @@
+import java.util.*;
 public class LinkedHashSets{
 public static void main(String[]args){
   // Preapre LinkedHashSets Object and Load Person Objects in it and iterate it using Iterator and While Loop
+  LinkedHashSet<Person> v=new LinkedHashSet<>();
+  v.add(new Person(1,"hello2",10000000));
+Iterator m=v.iterator();
+while(m.hasNext()){
+  System.out.println(m.next());
+}
+
+}
 }
