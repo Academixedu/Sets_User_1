06e2d88b383e9a0e908753179d2fb16164c07550
Vishal <87086820+Vishalprepro@users.noreply.github.com>
2024-08-29 18:45:44 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,9 +1,1 @@
-import java.util.*;
 
-public class test {
-    public static void main(String[] args) {
-        HashSet<Person> hs = new HashSet<Person>();
-        Person p = new Person(null, 0);
-
-    }
-}
