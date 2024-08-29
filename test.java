4702cde773bad4f1c606bd6dba38b0e81a6fd87f
Vishalprepro <adepuvishal17@gmail.com>
2024-08-29 18:30:01 +0530

    hello

---
+++ test.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class test {
+    public static void main(String[] args) {
+        HashSet<Person> hs = new HashSet<Person>();
+        Person p = new Person(null, 0);
+
+    }
+}
