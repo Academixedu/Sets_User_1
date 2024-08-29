5e254488c68a8279eef14021e8705a23413a2b68
Vishal <87086820+Vishalprepro@users.noreply.github.com>
2024-08-29 18:44:57 +0530

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,8 +1,13 @@
-public class Person {
-    // prepare a Person Data With Getters and Setters,Constructor,ToString and
-    // Override HashCode & Equals Methods in it
+import java.util.*;
+
+class Person1 {
     private String name;
-    private int age;
+    private int n;
+
+    public Person1(String name, int n) {
+        this.name = name;
+        this.n = n;
+    }
 
     public String getName() {
         return name;
@@ -12,17 +17,70 @@ public class Person {
         this.name = name;
     }
 
-    public int getAge() {
-        return age;
+    public int getN() {
+        return n;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public void setN(int n) {
+        this.n = n;
     }
 
     @Override
     public String toString() {
-        return "Person [name=" + name + ", age=" + age + "]";
+        return "Person1 [name=" + name + ", n=" + n + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + n;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Person1 other = (Person1) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (n != other.n)
+            return false;
+        return true;
+    }
+
+}
+
+public class Person {
+    // prepare a Person Data With Getters and Setters,Constructor,ToString and
+    // Override HashCode & Equals Methods in it
+    public static void main(String[] args) {
+        HashSet<Person1> hs = new HashSet<Person1>();
+        Person1 ps1 = new Person1("hello", 12);
+        Person1 ps2 = new Person1("hello", 12);
+        Person1 ps3 = new Person1("tara", 14);
+        ps1.setN(12);
+        ps1.setName("hello");
+        ps2.setN(12);
+        ps2.setName("hello");
+        ps3.setN(324);
+        ps3.setName("jsd");
+        hs.add(ps1);
+        hs.add(ps2);
+        hs.add(ps3);
+        for (Person1 persons : hs) {
+            System.out.println(persons);
+        }
+
     }
 
 }
