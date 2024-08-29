9f5ce97170de9c3ee67449aae59fa1930debe4dd
kandgule-dinesh <dineshkandagule@gmail.com>
2024-08-29 20:38:10 +0530

    change

--- HashSets.java
+++ HashSets.java
@@ -1,3 +1,15 @@
+import java.util.HashSet;
 public class HashSets{
 // Prepare Hashset object here and add person class objects in them iterate it by using for each
+
+    public static void main(String[] args) {
+        HashSet<Integer> p = new HashSet<>();
+        p.add(1);
+        p.add(2);
+        p.add(4);
+        p.add(3);
+        for (Integer integer : p) {
+            System.out.println(integer);
+        }
+    }
 }
--- Person.java
+++ Person.java
@@ -1,3 +1,23 @@
 public class Person{
-// prepare a Person Data With Getters and Setters,Constructor,ToString and Override HashCode & Equals Methods in it
-}
+    // prepare a Person Data With Getters and Setters,Constructor,ToString and Override HashCode & Equals Methods in it
+    int id;
+    String name;
+    public Person(int id, String name, int salary) {
+        this.id = id;
+        this.name = name;
+    }
+  
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+ 
+    @Override
+    public String toString() {
+        return "Person{id=" + id + ", name='" + name + "}";
+    }
+  }
